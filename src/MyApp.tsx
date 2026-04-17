72f65c1458cc782a0aeb237f5f631ae089ecf242
David-Brnb <321171197@ciencias.unam.mx>
2026-04-16 23:25:36 -0600

    changed main page

---
+++ src/MyApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyApp() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1>My App</h1>
+
+      <p>Hello, World!</p>
+    </div>
+  );
+}
+
+export default MyApp;
