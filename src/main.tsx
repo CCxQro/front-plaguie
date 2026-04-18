132b62c55bf1cfc8938b1f84ed1e6187aa039051
David-Brnb <321171197@ciencias.unam.mx>
2026-04-17 21:13:30 -0600

    add: Adeed routing to the web page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import MyApp from './MyApp.tsx'
-import Login from './Login.tsx'
+import Login from './Login'
+import MyApp from './MyApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<MyApp />} />
+        
+        {/* Redirección por defecto si no existe la ruta */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
