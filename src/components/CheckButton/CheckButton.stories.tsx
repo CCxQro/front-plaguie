c6c4042d8764cfeaeb799eb2ed2cb6c4485fd25c
David-Brnb <321171197@ciencias.unam.mx>
2026-04-17 20:29:12 -0600

    SCRUM-3 checkbutton component and implementation

---
+++ src/components/CheckButton/CheckButton.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { useState } from 'react';
+import CheckButton from './CheckButton';
+
+const meta = {
+  title: 'Example/CheckButton',
+  component: CheckButton,
+  tags: ['autodocs'],
+  argTypes: {
+    remember: { control: 'boolean' },
+    text: { control: 'text' },
+  },
+} satisfies Meta<typeof CheckButton>;
+
+export default meta;
+type Story = StoryObj<typeof CheckButton>;
+
+const CheckButtonWrapper = (args: any) => {
+  const [remember, setRemember] = useState(args.remember);
+
+  return (
+    <CheckButton 
+      {...args} 
+      remember={remember} 
+      setRemember={(val) => {
+        setRemember(val);
+        // Opcional: Esto permite que la pestaña "Actions" de Storybook registre el cambio
+        args.setRemember?.(val);
+      }} 
+    />
+  );
+};
+
+export const Default: Story = {
+  render: (args) => <CheckButtonWrapper {...args} />,
+  args: {
+    remember: false,
+    text: 'Recordar sesión',
+    width: 'w-4',
+    height: 'h-4',
+  },
+};
+
+export const CustomSize: Story = {
+  render: (args) => <CheckButtonWrapper {...args} />,
+  args: {
+    remember: true,
+    text: 'Aceptar términos',
+    width: 'w-6',
+    height: 'h-6',
+  },
+};
